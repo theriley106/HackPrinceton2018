b87f6dc2fffb82bc428db3750f1f922c63a5cb1d
Theriley106 <christopherlambert106@gmail.com>
2018-03-31 19:02:06 -0400

    Removed problematic file

--- raspi.py
+++ raspi.py
@@ -69,7 +69,6 @@ while True:
 	camera.capture(stream, format='png')
 	# "Rewind" the stream to the beginning so we can read its content
 	stream.seek(0)
-	file_like = cStringIO.StringIO(stream)
 	print stream.encode("base64")
 
 	img = Image.open(file_like)
