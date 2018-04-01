5c601fae3638d48f21372921de8c0455eae72165
Theriley106 <christopherlambert106@gmail.com>
2018-04-01 02:17:08 -0400

    Put into a try except loop for while loop in base.py

--- base.py
+++ base.py
@@ -7,16 +7,19 @@ URLs = ['http://10.24.88.66/html/cam_pic.php?time=12', "http://10.25.213.197/cam
 
 def checkForGun(url):
 	while True:
-		print("Checking {}".format(url))
-		response = requests.get(url, stream=True)
-		with open('file{}.png'.format(URLs.index(url)), 'wb') as out_file:
-		    shutil.copyfileobj(response.raw, out_file)
-		del response
-		with open("file{}.png".format(URLs.index(url)), "rb") as f:
-	   		data = f.read()
-	   	isGun = isAGun(data)
-	   	if isGun == True:
-	   		main.setAlarm(url)
+		try:
+			print("Checking {}".format(url))
+			response = requests.get(url, stream=True)
+			with open('file{}.png'.format(URLs.index(url)), 'wb') as out_file:
+			    shutil.copyfileobj(response.raw, out_file)
+			del response
+			with open("file{}.png".format(URLs.index(url)), "rb") as f:
+		   		data = f.read()
+		   	isGun = isAGun(data)
+		   	if isGun == True:
+		   		main.setAlarm(url)
+		except Exception as exp:
+			print exp
 
 
 threads = [threading.Thread(target=chekForGun, args=(url,)) for url in URLs]
