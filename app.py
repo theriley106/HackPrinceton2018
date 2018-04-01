b4bcac7a747de518b1643652f2092821d593151f
Theriley106 <christopherlambert106@gmail.com>
2018-04-01 02:19:02 -0400

    Added add number api endpoint

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ def main():
 def test():
 	return render_template('index.html')
 
-@app.route('/addNumber/<phoneNumber>')
-def addNumber():
-	return render_template('index.html')
+@app.route('/addNumber/<phoneNumber>' )
+def addNumber(phoneNumber):
+	main.writeNum(phoneNumber)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
