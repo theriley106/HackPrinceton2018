1f1ed1c90365819d9378ba25a47b138040ba5a6a
Theriley106 <christopherlambert106@gmail.com>
2018-04-01 02:18:02 -0400

    Added main dependancy in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ reload(sys)
 sys.setdefaultencoding('UTF8')
 from flask import Flask, request, jsonify, render_template, request, url_for, redirect, Markup, Response, send_file, send_from_directory, make_response
 app = Flask(__name__,template_folder="templates/",static_url_path='/static')
-
+import main
 
 @app.route('/')
 def main():
@@ -13,6 +13,10 @@ def main():
 
 @app.route('/test')
 def test():
+	return render_template('index.html')
+
+@app.route('/addNumber/<phoneNumber>')
+def addNumber():
 	return render_template('index.html')
 
 if __name__ == "__main__":
