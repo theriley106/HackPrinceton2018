bd55827d6fd7b76cbeaccc04b61acbda89f97697
Theriley106 <christopherlambert106@gmail.com>
2018-04-01 02:12:12 -0400

    Added check for gun

--- base.py
+++ base.py
@@ -1,7 +1,20 @@
 import requests
 import shutil
+import threading
 
 URLs = ['http://10.24.88.66/html/cam_pic.php?time=12', "http://10.25.213.197/cam_pic.php?time=0"]
+
+def checkForGun(url):
+	while True:
+		response = requests.get(url, stream=True)
+		with open('file.png', 'wb') as out_file:
+		    shutil.copyfileobj(response.raw, out_file)
+		del response
+		with open("file.png", "rb") as f:
+	   		data = f.read()
+	   	isGun = isAGun(data)
+		print isGun
+
 
 url = 'http://10.24.88.66/html/cam_pic.php?time=12'
 	response = requests.get(url, stream=True)
