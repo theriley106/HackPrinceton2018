cec5f270bc2900cf6a6c60319bf1afeb3736bea2
Theriley106 <christopherlambert106@gmail.com>
2018-04-01 01:54:13 -0400

    Converted main to if name run on direct

--- main.py
+++ main.py
@@ -42,43 +42,42 @@ def isAGun(imageString):
 
 def imgToNumpy(img):
 	return numpy.array(cv2.imencode('.png', img)[1]).tostring()
+if __name__ == '__main__':
+	while True:
 
-while True:
+		hog = cv2.HOGDescriptor()
+		hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-	cap = cv2.VideoCapture(0)
-	hog = cv2.HOGDescriptor()
-	hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+		_,img = cap.read()
 
-	_,img = cap.read()
+		# detect people in the image
+		(rects, weights) = hog.detectMultiScale(img, winStride=(4, 4),
+			padding=(8, 8), scale=1.05)
+		for (x, y, w, h) in rects:
+			cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-	# detect people in the image
-	(rects, weights) = hog.detectMultiScale(img, winStride=(4, 4),
-		padding=(8, 8), scale=1.05)
-	for (x, y, w, h) in rects:
-		cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-	# apply non-maxima suppression to the bounding boxes using a
-	# fairly large overlap threshold to try to maintain overlapping
-	# boxes that are still people
-	rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-	pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
-	if LOW_BANDWIDTH == True:
-		if len(pick) != 0:
-			isGun = isAGun(imgToNumpy(img))
+		# apply non-maxima suppression to the bounding boxes using a
+		# fairly large overlap threshold to try to maintain overlapping
+		# boxes that are still people
+		rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+		pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+		if LOW_BANDWIDTH == True:
+			if len(pick) != 0:
+				isGun = isAGun(imgToNumpy(img))
+			else:
+				isGun = False
 		else:
-			isGun = False
-	else:
-		isGun = isAGun(imgToNumpy(img))
-	print isGun
-	if isGun == True:
-		mqtt_client.publish("HP18/report/test", "SEND TEST raspi")
-	cap.release()
-	cv2.destroyAllWindows()
-	mqtt_client.disconnect() # disconnects client from broker
-	time.sleep(.01)
+			isGun = isAGun(imgToNumpy(img))
+		print isGun
+		if isGun == True:
+			mqtt_client.publish("HP18/report/test", "SEND TEST raspi")
+		cap.release()
+		cv2.destroyAllWindows()
+		mqtt_client.disconnect() # disconnects client from broker
+		time.sleep(.01)
 
 
 
 
-#if __name__ == '__main__':
+
 
