ce1ec6b0afaf9dbcedc5b472c155dd6df11e5786
Theriley106 <christopherlambert106@gmail.com>
2018-04-01 02:11:18 -0400

    Added base.py

---
+++ base.py
@@ -0,0 +1,14 @@
+import requests
+import shutil
+
+URLs = ['http://10.24.88.66/html/cam_pic.php?time=12', "http://10.25.213.197/cam_pic.php?time=0"]
+
+url = 'http://10.24.88.66/html/cam_pic.php?time=12'
+	response = requests.get(url, stream=True)
+	with open('file.png', 'wb') as out_file:
+	    shutil.copyfileobj(response.raw, out_file)
+	del response
+	with open("file.png", "rb") as f:
+   		data = f.read()
+   	isGun = isAGun(data)
+	print isGun
