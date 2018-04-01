de9920ad3da631c8ce00592f92d5d64f5986f205
Theriley106 <christopherlambert106@gmail.com>
2018-04-01 02:13:13 -0400

    Added custom file name

--- base.py
+++ base.py
@@ -7,16 +7,16 @@ URLs = ['http://10.24.88.66/html/cam_pic.php?time=12', "http://10.25.213.197/cam
 def checkForGun(url):
 	while True:
 		response = requests.get(url, stream=True)
-		with open('file.png', 'wb') as out_file:
+		with open('file{}.png'.format(URLs.index(url)), 'wb') as out_file:
 		    shutil.copyfileobj(response.raw, out_file)
 		del response
-		with open("file.png", "rb") as f:
+		with open("file{}.png".format(URLs.index(url)), "rb") as f:
 	   		data = f.read()
 	   	isGun = isAGun(data)
-		print isGun
+	   	if isGun == True:
 
 
-url = 'http://10.24.88.66/html/cam_pic.php?time=12'
+
 	response = requests.get(url, stream=True)
 	with open('file.png', 'wb') as out_file:
 	    shutil.copyfileobj(response.raw, out_file)
