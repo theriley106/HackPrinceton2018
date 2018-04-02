da015789bc32ad7cc2cbff17d624235cbad09ade
Theriley106 <christopherlambert106@gmail.com>
2018-04-02 18:04:28 -0400

    AzureText -> msAzure"

--- msAzure.py
+++ msAzure.py
@@ -3,8 +3,6 @@ import requests
 subscription_key = open("../mkey.txt").read().strip()
 image_url = 'https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/images/family_photo.jpg'
 vision_analyze_url = 'https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze'
-
-
 headers  = {'Ocp-Apim-Subscription-Key': subscription_key }
 params   = {'visualFeatures': 'Categories,Description,Color'}
 data     = {'url': image_url}
