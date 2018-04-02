78270d045d3ec735807ad5b2f2d8791b1d1b9f9a
Theriley106 <christopherlambert106@gmail.com>
2018-04-02 18:03:46 -0400

    Added working azure test

--- AzureTest.py
+++ AzureTest.py
@@ -1,14 +1,14 @@
 import requests
 
 subscription_key = open("../mkey.txt").read().strip()
+image_url = 'https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/images/family_photo.jpg'
+vision_analyze_url = 'https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze'
 
-headers  = {'Ocp-Apim-Subscription-Key': subscription_key}
-params   = {'model': 'landmarks'}
+
+headers  = {'Ocp-Apim-Subscription-Key': subscription_key }
+params   = {'visualFeatures': 'Categories,Description,Color'}
 data     = {'url': image_url}
-response = requests.post(landmark_analyze_url, headers=headers, params=params, json=data)
+response = requests.post(vision_analyze_url, headers=headers, params=params, json=data)
 response.raise_for_status()
-
-analysis      = response.json()
-assert analysis["result"]["landmarks"] is not []
-
-landmark_name = analysis["result"]["landmarks"][0]["name"].capitalize()
+analysis = response.json()
+print analysis
