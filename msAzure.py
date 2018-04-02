7ec1f15766582aaa43aa41ea1ff06019643e640b
Theriley106 <christopherlambert106@gmail.com>
2018-04-02 18:05:07 -0400

    Added if name == main to ms Azure

--- msAzure.py
+++ msAzure.py
@@ -1,12 +1,15 @@
 import requests
 
-subscription_key = open("../mkey.txt").read().strip()
-image_url = 'https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/images/family_photo.jpg'
-vision_analyze_url = 'https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze'
-headers  = {'Ocp-Apim-Subscription-Key': subscription_key }
-params   = {'visualFeatures': 'Categories,Description,Color'}
-data     = {'url': image_url}
-response = requests.post(vision_analyze_url, headers=headers, params=params, json=data)
-response.raise_for_status()
-analysis = response.json()
-print analysis
+
+
+if __name__ == '__main__':
+	subscription_key = open("../mkey.txt").read().strip()
+	image_url = 'https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/images/family_photo.jpg'
+	vision_analyze_url = 'https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze'
+	headers  = {'Ocp-Apim-Subscription-Key': subscription_key }
+	params   = {'visualFeatures': 'Categories,Description,Color'}
+	data     = {'url': image_url}
+	response = requests.post(vision_analyze_url, headers=headers, params=params, json=data)
+	response.raise_for_status()
+	analysis = response.json()
+	print analysis
